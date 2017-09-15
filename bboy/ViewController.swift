b5b5352b9280ee7928403d1c38c120891486af97
Justin Koh <justinkoh@Justins-MacBook-Pro.local>
2017-09-15 12:01:10 -0400

    FUNCTIONAL

--- bboy/ViewController.swift
+++ bboy/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import SceneKit
 import ARKit
+import Foundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var animations = [String: CAAnimation]()
+    var idle:Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +27,85 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        //load animations
+        loadAnimations()
     }
     
+    func loadAnimations(){
+        //Load the character in the idle animation
+        let idleScene = SCNScene(named: "art.scnassets/idleFixed.dae")!
+        
+        //This node will be parent of all the animation models
+        let node = SCNNode()
+        
+        // Add all the child nodes to the parent node
+        for child in idleScene.rootNode.childNodes {
+            node.addChildNode(child)
+        }
+        //set up properties
+        node.position = SCNVector3(0, -1, -4)
+        node.scale = SCNVector3(0.01, 0.01, 0.01)
+
+        //Add the node to the scene
+        sceneView.scene.rootNode.addChildNode(node)
+
+        //Load all the DAE animations
+        loadAnimation(withKey: "dancing", sceneName: "art.scnassets/footworkFixed", animationIdentifier: "footworkFixed-1")
+
+    }
+
+    func loadAnimation(withKey: String, sceneName: String, animationIdentifier: String){
+        let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae")
+        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
+
+        if let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self){
+            //animation only plays once
+            animationObject.repeatCount = 1
+            //to create smooth transition between animations
+            animationObject.fadeInDuration = CGFloat(1)
+            animationObject.fadeOutDuration = CGFloat(0.5)
+
+            //store animation for later use
+            animations[withKey] = animationObject
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: sceneView)
+
+        //test if object was touched
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+
+        let hitResults: [SCNHitTestResult] = sceneView.hitTest(location, options:hitTestOptions)
+        if hitResults.first != nil {
+            if(idle){
+                playAnimation(key: "dancing")
+            }
+            else{
+                stopAnimation(key: "dancing")
+            }
+        }
+        idle = !idle
+        return
+    }
+
+    func playAnimation(key: String){
+        // Add the animation to start playing it right away
+        sceneView.scene.rootNode.addAnimation(animations[key]!, forKey: key)
+    }
+
+    func stopAnimation(key: String){
+        //Stop the animation with a smooth transition
+        sceneView.scene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
